107c47bc4c4655add0fe9f5a492245743b126029
balletFrancois18 <144659597+balletFrancois18@users.noreply.github.com>
2024-10-17 10:11:40 +0000

    Exercice 19*

---
+++ Exercice19.php
@@ -0,0 +1,23 @@
+<?php
+function capital($pays) {
+    switch ($pays) {
+        case 'France':
+            return 'Paris';
+        case 'Allemagne':
+            return 'Berlin';
+        case 'Italie':
+            return 'Rome';
+        case 'Maroc':
+            return 'Rabat';
+        case 'Espagne':
+            return 'Madrid';
+        case 'Portugal':
+            return 'Lisbonne';
+        case 'Angleterre':
+            return 'Londres';
+        default:
+            return 'Inconnu';
+    }
+}
+
+
